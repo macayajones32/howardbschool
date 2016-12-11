e3de8049afe3c67da61bb41844e7624d51c4e36b
Simeon Kakpovi <skakpovi@gmail.com>
2016-12-11 14:01:11 -0500

    added context to rended partial links

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,11 @@ $(document).ready(function(){'use strict';$(window).scroll(function(){if($(this)
 function toggleChevron1(e){$(e.target).prev('.panel-heading').find("i.indicator").toggleClass('fa-angle-down fa-angle-right');}
 $('.accordion_arrow').on('hidden.bs.collapse',toggleChevron1);$('.accordion_arrow').on('shown.bs.collapse',toggleChevron1);$('.accordion_plusminus').on('hidden.bs.collapse',toggleChevron);$('.accordion_plusminus').on('shown.bs.collapse',toggleChevron);});
 
-/**************side nav------------------------------****/
-    // is the navigation panel open?
-       
+        /**************side nav------------------------------****/
+        // is the navigation panel open?
         var status = 0;
-                /* Set the width of the side navigation to 250px and add a black background color to body */
+
+        /* Set the width of the side navigation to 250px and add a black background color to body */
         function openNav(e) {
             status = 1;
             $('.side-tabs').css({
@@ -17,16 +17,15 @@ $('.accordion_arrow').on('hidden.bs.collapse',toggleChevron1);$('.accordion_arro
             document.getElementById("cover").classList.add("overlay");
             
             /**** setting which navigation will appear*/
-            count = introspect();
-            pre = "";
-            for (var i=0; i<count; i++){
-                pre += "../"
-            } //building context for relative links
+            pre = introspect(); 
             nav = e.childNodes[1].id; //this is the sideNav file we want
             partial = '<a class="closebtn" href="javascript:void(0)" onclick="closeNav()">&times;</a>'
             partial += '<div id="menu-content" w3-include-html="' + pre + 'partials/'+ nav +'.html"></div>'
             document.getElementById("mySidenav").innerHTML = partial
             w3IncludeHTML();
+            
+            
+            adjust_links()
         }
 
         /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -92,13 +91,49 @@ $('.accordion_arrow').on('hidden.bs.collapse',toggleChevron1);$('.accordion_arro
         }
 
 
-        /**************Call menus!------------------------------****/
+        //Call menus!
          w3IncludeHTML();
 
+        //Adjust menu links 
+        function adjust_links(){
+            var menu = document.getElementById("menu-content");
+            console.log(menu);
+        }
 
-        /******* Page awareness ********/
+        
+        // Page awareness 
+        //returns depth of page
         function introspect(){
             var link = document.getElementsByTagName("link")[0].getAttribute("href");
             var count = (link.match(/\.\.\//g) || []).length;
-            return count;
+            pre = "";
+            for (var i=0; i<count; i++){
+                pre += "../"
+            } //building context for relative links
+            
+            return pre;
+        }
+
+        function w3IncludeHTML() {
+          pre = 'href="' + introspect();
+          var z, i, a, file, xhttp;
+          z = document.getElementsByTagName("*");
+          for (i = 0; i < z.length; i++) {
+            if (z[i].getAttribute("w3-include-html")) {
+              a = z[i].cloneNode(false);
+              file = z[i].getAttribute("w3-include-html");
+              xhttp = new XMLHttpRequest();
+              xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                  a.removeAttribute("w3-include-html");
+                  a.innerHTML = this.responseText.replace(/href="\[pre\]/g, pre);
+                  z[i].parentNode.replaceChild(a, z[i]);
+                  w3IncludeHTML();
+                }
+              }      
+              xhttp.open("GET", file, true);
+              xhttp.send();
+              return;
+            }
+          }
         }
